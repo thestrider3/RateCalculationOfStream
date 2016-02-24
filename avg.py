f3351d78e7993956bb97ab27839ec51ccf098273
thestrider3 <tulikabhatt92@gmail.com>
2016-02-24 16:54:40 -0500

    comments added

--- avg.py
+++ avg.py
@@ -1,4 +1,5 @@
-
+#The average rate calculation in this file is done by the code snippet demonstrated by Professor Mike Dewar in the Class Lecture.
+#Tweepy Library is imported as it provided easy methods for authentication as well as posting tweets programmatically
 import redis
 import json
 import time
@@ -13,6 +14,8 @@ while 1:
     pipe = conn.pipeline()
 
     keys = conn.keys()
+    #I have added this line as sometimes keys expire in Redis before they could be accessed and hence lead to a null value
+    #Now if this value is again to find out deltas, then they give a TypeError exception
     if not keys:
     	keys=[]
 
@@ -23,7 +26,8 @@ while 1:
     except TypeError:
         print keys
         continue
-
+#Here a moving average is created by taking the sum of the deltas and diving by length. The deltas keep on increasing as
+#data arrives in twitter_getdata.py and due to the use of pipes.
     if len(deltas):
         rate = sum(deltas)/float(len(deltas))
     else:
@@ -32,7 +36,8 @@ while 1:
     auth.set_access_token("ENTER YOUR ACCESS TOKEN" ,"ENTER YOUR SECRET ACCESS TOKEN")
     api = tweepy.API(auth)
     print api.me().name
+    #I have added a count variable as Twitter doesn't allow to print duplicate rates which may occur if tweets arrive at the same time
     count=count+1
-    tweet = "The rate of "+str(count)+"is "+str(rate)
+    tweet = str(count)+" rate : "+str(rate)
     status = api.update_status(status=tweet)
     
