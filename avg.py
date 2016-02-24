3122899ed9d4c8afde5ba5027d4e2a836d5ddc74
thestrider3 <tulikabhatt92@gmail.com>
2016-02-24 17:08:44 -0500

    rates update

--- avg.py
+++ avg.py
@@ -22,22 +22,22 @@ while 1:
     values = conn.mget(keys)
 
     try:
-        deltas = [float(v) for v in values]
+        rates = [float(v) for v in values]
     except TypeError:
         print keys
         continue
 #Here a moving average is created by taking the sum of the deltas and diving by length. The deltas keep on increasing as
 #data arrives in twitter_getdata.py and due to the use of pipes.
-    if len(deltas):
-        rate = sum(deltas)/float(len(deltas))
+    if len(rates):
+        avg_rate = sum(rates)/float(len(rates))
     else:
-        rate = 0
+        avg_rate = 0
     auth=tweepy.OAuthHandler("ENTER YOUR API KEY","ENTER YOUR API SECRET")
     auth.set_access_token("ENTER YOUR ACCESS TOKEN" ,"ENTER YOUR SECRET ACCESS TOKEN")
     api = tweepy.API(auth)
     print api.me().name
     #I have added a count variable as Twitter doesn't allow to print duplicate rates which may occur if tweets arrive at the same time
     count=count+1
-    tweet = str(count)+" rate : "+str(rate)
+    tweet = str(count)+" rate : "+str(avg_rate)
     status = api.update_status(status=tweet)
     
