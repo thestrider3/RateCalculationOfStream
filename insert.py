d136526c195200ec20a697d4052bd3ec66202d4a
thestrider3 <tulikabhatt92@gmail.com>
2016-02-24 16:27:17 -0500

    comments added

--- insert.py
+++ insert.py
@@ -1,3 +1,7 @@
+#This code snippted was taken from the Class Lecture taught by Professor Mike Dewar. In this file, the data obtained from 
+#delta.py is parsed using JSON and the time and the difference delta is stored into Redis. The time is made the key as it 
+#is unique and delta is attached to it as its value. A key expiration rate of 120 s is set in the method conn.setex() 
+#which is sufficient for our purpose.
 import json
 import sys
 import redis
